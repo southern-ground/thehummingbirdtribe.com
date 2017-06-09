d0f2953d51f4508723d86ce24dfbdc026b4147d1
Ferris Thomas <fsthomas@gmail.com>
2017-06-09 15:44:31 -0400

    Whitespace. Adds stubbed out Google tracking. Adds updated background image. Adds form validation on the email form. Adds meta tags to header.

--- src/js/main.js
+++ src/js/main.js
@@ -5,36 +5,40 @@ var hbt,
     HBT = function (w) {
         this.lazyload = null;
         this.$w = $(window);
+        this.$signUpForm = $('#sign-up');
+        this.$emailField = $('#email');
+        this.$errorMsg = $('#error-message');
     };
 HBT.prototype = {
     init: function () {
         console.log('hbt');
         this.lazyload = new LazyLoad();
-        this.$w.on('resize', function () {
-            hbt.resize();
+        this.$signUpForm.on('submit', function(e){
+            hbt.$signUpForm.removeClass('form-error');
+            hbt.$errorMsg.addClass('hidden');
+
+            var validEmail = function (email) {
+                var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                return regEx.test(email);
+            };
+
+            if (validEmail(hbt.$emailField.val())) {
+                // Submit the form:
+                console.log('\tSubmit the form');
+            } else {
+                hbt.$signUpForm.addClass('form-error');
+                hbt.$errorMsg.removeClass('hidden');
+            }
+
+            e.preventDefault();
+            e.stopPropagation();
+            void(0);
+            return false;
+
         });
-        this.resize();
     },
     resize: function () {
-        console.log('hbt::resize', window.devicePixelRatio);
-        console.log(this.$w.innerWidth() + ' x ' + this.$w.innerHeight());
 
-        /*var win = {
-            w: this.$w.innerWidth(),
-            h: this.$w.innerHeight()
-        }
-
-        if (win.w < 1024) {
-            var scalar = 1 - (win.w - 400) / 624,
-                css = {
-                    'left': 1-scalar*83,
-                    'top': 1-scalar*44
-                };
-
-            console.log(css);
-
-            $('.branch--upperLeft').css(css);
-        }*/
     }
 };
 $(document).ready(function () {
